34868ba2ab099828e3bcaebc28ce6f728adf97a8
Lee Campbell <lee.campbell@evergreen-life.co.uk>
2020-02-16 20:26:40 +0000

    v9.0.3

---
+++ dist/src/bsonTypeOfSchema.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var typeOfSchema_1 = require("./typeOfSchema");
+// a map to convert BSON Types to JSON Types as per:
+// https://docs.mongodb.com/manual/reference/operator/query/type/#document-type-available-types
+// where MongoDB $types are missing from the map, they have no appropriate JSON Schema type, and 
+// will be handled by the default type-mapping. 
+var bsonTypeToJsonType = new Map();
+bsonTypeToJsonType.set("double", "number");
+bsonTypeToJsonType.set("binData", "string"); // need to consider `contentEncoding` and `contentMediaType`
+bsonTypeToJsonType.set("bool", "boolean");
+bsonTypeToJsonType.set("int", "number");
+bsonTypeToJsonType.set("long", "number");
+bsonTypeToJsonType.set("objectId", "any");
+function bsonTypeOfSchema(schema) {
+    if (!schema.bsonType)
+        return typeOfSchema_1.typeOfSchema(schema);
+    switch (schema.bsonType) {
+        default:
+            if (!schema.type && bsonTypeToJsonType.get(schema.bsonType) !== undefined) {
+                schema.type = bsonTypeToJsonType.get(schema.bsonType);
+            }
+            return typeOfSchema_1.typeOfSchema(schema);
+    }
+}
+exports.bsonTypeOfSchema = bsonTypeOfSchema;
+//# sourceMappingURL=bsonTypeOfSchema.js.map
